d7a3cd59ab632e169b57d88edc7a6b54e9eac9ec
Kasa Pavan <pavansyamala2003@gmail.com>
2023-06-25 07:11:49 +0530

    pipeline

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np 
 import pandas as pd
 import pickle
+import dill
 from sklearn.metrics import r2_score 
 
 from src.exception import CustomException
@@ -34,5 +35,11 @@ def evaluvate_model(x_train,y_train,x_test,y_test,models):
             report[list(models.keys())[i]] = test_model_score 
 
         return report 
+    except Exception as e :
+        raise CustomException(e,sys)
+def load_object(file_path):
+    try :
+        with open(file_path , 'rb') as file_obj :
+            return dill.load(file_obj) 
     except Exception as e :
         raise CustomException(e,sys)
